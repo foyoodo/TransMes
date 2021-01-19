8530ea6a4f96e3183be727efb89ec87725d4e454
foyoodo <foyoodo@gmail.com>
2021-01-19 17:41:33 +0800

    Add a TextField to send mes

--- TransMes/ContentView.swift
+++ TransMes/ContentView.swift
@@ -8,11 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var input = ""
     var body: some View {
         TabView {
             NavigationView {
                 VStack {
-                    Text("小译")
+                    Spacer()
+                    HStack {
+                        ZStack {
+                            Capsule()
+                                .fill(Color("BlankDetailColor"))
+                                .frame(minHeight: 34, alignment: .bottom)
+                            HStack {
+                                TextField("在这里输入文本", text: $input)
+                                    .background(Color("BlankDetailColor"))
+                                    .onAppear { self.input = "" }
+                                ZStack {
+                                    Circle()
+                                        .frame(width: 30, height: 30)
+                                        .foregroundColor(.blue)
+                                    Button(action: {}, label: {
+                                        Image(systemName: "arrow.up")
+                                    })
+                                    .foregroundColor(Color.white)
+                                }
+                            }
+                            .padding(.trailing, 5)
+                            .padding(.leading, 17)
+                        }
+                    }
+                    .padding(10)
+                    .frame(height: 60)
+                    .border(Color("BlankDetailColor"))
                 }
                 .navigationBarTitle("小译", displayMode: .automatic)
                 .navigationBarItems(trailing: NavigationLink(destination: ConfigView()) {
