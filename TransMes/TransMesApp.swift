cc061c5460d05ffa72575122a665efc6caffa130
foyoodo <foyoodo@gmail.com>
2021-01-19 15:06:18 +0800

    Initial Commit

---
+++ TransMes/TransMesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransMesApp.swift
+//  TransMes
+//
+//  Created by foyoodo on 2021/1/19.
+//
+
+import SwiftUI
+
+@main
+struct TransMesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
